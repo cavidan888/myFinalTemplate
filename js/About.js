82f8c636e835d13dbc45e336b43bddb6b4b8d837
Cavidan <acavidan.p102@code.edu.az>
2017-08-05 22:28:52 +0400

    six commit

--- js/About.js
+++ js/About.js
@@ -9,16 +9,32 @@ myHover.style.marginBottom = "50px";
 
 
 myHover.addEventListener("mouseover", function(){
-	this.style.backgroundColor = "#53B778";
+	 this.style.backgroundColor = "#53B778";
 	 this.style.transition = "0.2s ease-in";
 	 this.style.color="white";
 	
-})
+});
 
 
 myHover.addEventListener("mouseleave", function(){
-	this.style.backgroundColor = "rgba(0,0,0,0.0)";
+	 this.style.backgroundColor = "rgba(0,0,0,0.0)";
 	 this.style.transition = "0.2s ease-in";
 	 this.style.color="white";
 	
-})
+});
+
+
+$(".iconDiv").bind("mouseover",function(){
+	$(this).css("background", "#53B778");
+	$(this).css("transition", "0.1s ease-in");
+	$(this).css("color", "white");
+
+});
+
+$(".iconDiv").bind("mouseleave",function(){
+	$(this).css("background", "lightgray");
+	$(this).css("transition", "0.1s ease-in");
+	$(this).css("color", "black");
+});
+
+ 
