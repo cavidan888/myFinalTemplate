3392d906467307a45f5ef66f2b3356c33db0a12c
Cavidan <acavidan.p102@code.edu.az>
2017-08-05 04:03:27 +0400

    second commit

--- js/script.js
+++ js/script.js
@@ -11,3 +11,31 @@ for (var i = 0; i < progressbar.length; i++) {
 		}
 	})
 }
+
+
+
+
+var myHover = document.querySelector(".dmColInput");
+myHover.style.backgroundColor = "rgba(0,0,0,0.0)";
+myHover.style.padding = "10px";
+myHover.style.border = "2px solid #53B778 ";
+myHover.style.color = "white";
+myHover.style.marginBottom = "50px";
+
+
+
+
+myHover.addEventListener("mouseover", function(){
+	this.style.backgroundColor = "#53B778";
+	 this.style.transition = "0.2s ease-in";
+	 this.style.color="white";
+	
+})
+
+
+myHover.addEventListener("mouseleave", function(){
+	this.style.backgroundColor = "rgba(0,0,0,0.0)";
+	 this.style.transition = "0.2s ease-in";
+	 this.style.color="white";
+	
+})
